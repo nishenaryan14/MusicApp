fb810ca6968f054252c45a853106cd89aa3abed4
Aryan Nishen <127716401+nishenaryan14@users.noreply.github.com>
2023-07-30 19:50:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
 
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html");
-});
+// app.get("/", function(req, res){
+//     res.sendFile(__dirname+"/index.html");
+// });
 
-app.get("/login",(req, res)=>{
-    res.sendFile(__dirname+"/login.html");
-});
+// app.get("/login",(req, res)=>{
+//     res.sendFile(__dirname+"/login.html");
+// });
 
-app.post("/login",(req, res)=>{
-    console.log("you have successfully logged in");
-    res.redirect("/")
-})
+// app.post("/login",(req, res)=>{
+//     console.log("you have successfully logged in");
+//     res.redirect("/")
+// })
 
 
 
-app.get("/home",(req, res)=>{
-    res.sendFile(__dirname+"/home.html");
-})
-app.listen(3000, function(){
-    console.log("server started on port 3000");
-});
+// app.get("/home",(req, res)=>{
+//     res.sendFile(__dirname+"/home.html");
+// })
+// app.listen(3000, function(){
+//     console.log("server started on port 3000");
+// });
